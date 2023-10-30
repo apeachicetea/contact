cd7a4cfbe7516b839755f285b41fd0bb4f94e2ac
apeachicetea <eodbszla@gmail.com>
2023-10-30 12:59:37 -0500

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // 내 자식 위젯의 기준점을 중앙으로 설정해주는 Center()
-      home: Center(child: Container(width: 50, height: 50, color: Colors.blue,))
-    );
+        // 내 자식 위젯의 기준점을 중앙으로 설정해주는 Center()
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Application'),
+      ),
+      body: Text('Hello World!'),
+      bottomNavigationBar: BottomAppBar(
+          child: Container(
+        width: double.infinity,
+        height: 55,
+        decoration: BoxDecoration(
+            border:
+                Border(top: BorderSide(color: Theme.of(context).dividerColor))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.info),
+          ],
+        ),
+      )),
+    ));
   }
 }
